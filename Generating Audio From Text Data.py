9e9f891f1c314e9cb8247a4b87bfed65e3a3a5f9
sarangsvkm <sarangsvkm@gmail.com>
2022-03-02 09:40:47 +0530

    Generating Audio From Text Data

--- Generating Audio From Text Data.py
+++ Generating Audio From Text Data.py
@@ -4,4 +4,4 @@ text=open('deno.txt','r',encoding='utf-8').read()
 language='hi'
 output=gTTS(text=text,lang=language,slow=False)
 output.save('fileout.mp4')
-os.system('start fileoutput.mp4')
+os.system('start fileout.mp4')
